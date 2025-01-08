de283f6af3d90d0058ae29d842281f1f3a3fe0d1
LogLot <prattlog3@gmail.com>
2025-01-07 22:41:38 -0500

    trying different node nonsense, and added ya2p music

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
   const {protocol, app, BrowserWindow } = require('electron')
+  const path = require('path');
+  const fs = require('fs');
+
+  
 const createWindow = () => {
 const win = new BrowserWindow({
   width: 192000,
   height: 108000,
   transparent: true, frame: false
 })
-win.setIgnoreMouseEvents(true)
+// win.setIgnoreMouseEvents(true)
 win.setAlwaysOnTop(true, 'screen');
   win.loadFile('index.html')
-  win.setFullScreen(true)
+  // win.setFullScreen(true)
 }
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
@@ -19,3 +23,21 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
+
+
+
+
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'Documents');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
--- musicMan.js
+++ musicMan.js
@@ -1,10 +1,43 @@
+
+
+
 const canvas = document.getElementById('music');
 const ctx = canvas.getContext('2d');
+var width = 0
+var height = 0
+var gradX = 0
+var gradY = 0
+var targGradX = 150
+var targGradY = 300
 function resizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  width = window.innerWidth
+  height = window.innerHeight
+
 }
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
-ctx.fillStyle = "#fff"
-ctx.fillRect(50,50,50,50)
+
+function draw(){
+  const gradient = ctx.createLinearGradient(0, 0, gradX, gradY);
+  gradient.addColorStop(0, "#1f1f1f");
+  gradient.addColorStop(.96999, "#1f1f1f");
+  gradient.addColorStop(.97, "#0f0f0f");
+  gradient.addColorStop(.99999, "#0f0f0f");
+  gradient.addColorStop(1, "#0000");
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, width, height);
+}
+
+function tick(){
+  draw()
+  gradX = ((gradX*10)+targGradX)/11
+  gradY = ((gradY*10)+targGradY)/11
+  requestAnimationFrame(tick)
+}
+
+requestAnimationFrame(tick)
+
+
+//requiring path and fs modules
