5414fa8c809f1141921e41f64e40050202c86893
loglot <88983354+loglot@users.noreply.github.com>
2025-01-08 19:23:02 +0000

    added basic color changing based on state

--- musicMan.js
+++ musicMan.js
@@ -7,13 +7,13 @@ var width = 0
 var height = 0
 var gradX = 1
 var gradY = 1
-var targGradX = 1
-var targGradY = 1
+var targGradX = 18
+var targGradY = 18
 var latch = true
-var state = "playing"
+var state = "paused"
 
-var targetColor = [31,31,31]
-var Color = [31,31,31]
+var targetColor = [0,0,0]
+var Color = [155,155,155]
 
 var playingText = "place of holding"
 function resizeCanvas() {
@@ -29,22 +29,35 @@ window.addEventListener('resize', resizeCanvas);
 function draw(){
   ctx.clearRect(0, 0, width, height); 
   const gradient = ctx.createLinearGradient(0, 0, gradX, gradY);
-  gradient.addColorStop(0, `rgb(${(Color[0]-16)+","+(Color[1]-16)+","+(Color[2]-16)})`);
-  gradient.addColorStop(.96999, `rgb(${Color[0]+","+Color[1]+","+Color[2]})`);
+  gradient.addColorStop(0, `rgb(${(Color[0]-26)+","+(Color[1]-26)+","+(Color[2]-26)})`);
+  gradient.addColorStop(.96999, `rgb(${(Color[0]+16)+","+(Color[1]+16)+","+(Color[2]+16)})`);
+  gradient.addColorStop(.5, `rgb(${Color[0]+","+Color[1]+","+Color[2]})`);
   gradient.addColorStop(.97, `rgb(${Color[0]+","+Color[1]+","+Color[2]})`);
-  gradient.addColorStop(.99999, `rgb(${(Color[0]-16)+","+(Color[1]-16)+","+(Color[2]-16)})`);
+  gradient.addColorStop(.99999, `rgb(${(Color[0]-26)+","+(Color[1]-26)+","+(Color[2]-26)})`);
   gradient.addColorStop(1, "#0000");
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, width, height);
   ctx.fillStyle = "#fff";
   ctx.font = "48px serif";
-  ctx.fillText(`Now Playing ${playingText}`, -990+gradY*3, 50);
+  ctx.fillText(`> ${playingText}`, -990+gradY*3.4, -100+gradX);
 }
 
 function tick(){
+  if(state=="playing"){
+    targetColor = [41,91,21]
+  }
+  if(state=="paused"){
+    targetColor = [31,31,31]
+  }
+  if(state=="error"){
+    targetColor = [91,41,21]
+  }
   draw()
   gradX = ((gradX*30)+targGradX)/31
   gradY = ((gradY*30)+targGradY)/31
+  for(let i = 0; i<3;i++){
+    Color[i]=((Color[i]*50)+targetColor[i])/51
+  }
   console.log(gradX, targGradX)
   trueTicker()
   requestAnimationFrame(tick)
@@ -56,6 +69,7 @@ async function trueTicker(){
       targGradY = 300
       console.log(gradX, targGradX)
       latch=false
+      state = "playing"
       var rand=Math.floor(Math.random()*window.songList.length)
       var audio = new Audio(`./music/${window.songList[rand]}`);
       playingText = window.songList[rand]
