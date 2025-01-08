ff2344fd2df1d863bfb176cada91fbc198443f7d
LogLot <prattlog3@gmail.com>
2025-01-08 00:25:40 -0500

    finalized v 1.0

--- main.js
+++ main.js
@@ -2,17 +2,31 @@
   const path = require('path');
   const fs = require('fs');
 
-  
+
 const createWindow = () => {
 const win = new BrowserWindow({
   width: 192000,
   height: 108000,
   transparent: true, frame: false
 })
-// win.setIgnoreMouseEvents(true)
+win.setIgnoreMouseEvents(true)
 win.setAlwaysOnTop(true, 'screen');
   win.loadFile('index.html')
-  // win.setFullScreen(true)
+   win.setFullScreen(true)
+  // win.webContents.on('did-finish-load', () => {
+    
+const directoryPath = path.join(__dirname, './music');
+var songList = []
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    files.forEach(function (file) {
+      songList[songList.length]=`"${file}"`
+    });
+    console.log(songList); 
+    win.webContents.executeJavaScript(`window.songList=[${songList}]`)
+});
 }
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
@@ -26,18 +40,3 @@ app.whenReady().then(() => {
 
 
 
-
-//joining path of directory 
-const directoryPath = path.join(__dirname, 'Documents');
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
-    });
-});
--- musicMan.js
+++ musicMan.js
@@ -5,10 +5,13 @@ const canvas = document.getElementById('music');
 const ctx = canvas.getContext('2d');
 var width = 0
 var height = 0
-var gradX = 0
-var gradY = 0
-var targGradX = 150
-var targGradY = 300
+var gradX = 1
+var gradY = 1
+var targGradX = 1
+var targGradY = 1
+var latch = true
+
+var playingText = "place of holding"
 function resizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -20,6 +23,7 @@ resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
 
 function draw(){
+  ctx.clearRect(0, 0, width, height); 
   const gradient = ctx.createLinearGradient(0, 0, gradX, gradY);
   gradient.addColorStop(0, "#1f1f1f");
   gradient.addColorStop(.96999, "#1f1f1f");
@@ -28,16 +32,47 @@ function draw(){
   gradient.addColorStop(1, "#0000");
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = "#fff";
+  ctx.font = "48px serif";
+  ctx.fillText(`Now Playing ${playingText}`, -990+gradY*3, 50);
 }
 
 function tick(){
   draw()
-  gradX = ((gradX*10)+targGradX)/11
-  gradY = ((gradY*10)+targGradY)/11
+  gradX = ((gradX*30)+targGradX)/31
+  gradY = ((gradY*30)+targGradY)/31
+  console.log(gradX, targGradX)
+  trueTicker()
   requestAnimationFrame(tick)
+}
+async function trueTicker(){
+  if(window.songList){
+    if(latch){
+      targGradX = 150
+      targGradY = 300
+      console.log(gradX, targGradX)
+      latch=false
+      var rand=Math.floor(Math.random()*window.songList.length)
+      var audio = new Audio(`./music/${window.songList[rand]}`);
+      playingText = window.songList[rand]
+      audio.play()
+      await sleep(3000)
+      targGradX = 18
+      targGradY = 18
+      await sleep(audio.duration*1000-3000)
+      latch=true
+
+
+    }
+  }
+}
+async function timedplay(params) {
+  
 }
 
 requestAnimationFrame(tick)
 
-
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 //requiring path and fs modules
